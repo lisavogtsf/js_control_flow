2352e846a908e73691882c62b35526da25732f81
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-08 16:24:18 -0700

    working on reverse

---
+++ reverse.js
@@ -0,0 +1,13 @@
+var inputString = "building"
+
+// reverse it 
+
+for (var i = 0; i < inputString.length; i++) {
+	//split by empty string
+	var reverseArray = inputString.split('');
+
+	// treat string as an array
+	// var reverseString[i] = inputString[inputString.length - (i + 1)];
+}
+console.log(reverseArray);
+//console.log(reverseString);
