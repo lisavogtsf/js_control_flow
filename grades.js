310868f1b33b0735dfcf18f02fec2f052b88144a
Lisa Vogt <lisavogt.sf@gmail.com>
2014-07-08 17:05:35 -0700

    grades done

---
+++ grades.js
@@ -0,0 +1,18 @@
+// grades.js
+var number_score = 74;
+
+if (number_score > 89) {
+	console.log("A");
+}
+else if (number_score > 79) {
+	console.log("B");
+}
+else if (number_score > 69) {
+	console.log("C");
+}
+else if (number_score > 59) {
+	console.log("D");
+}
+else {
+	console.log("F");
+}
